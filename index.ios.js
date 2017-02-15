a4ee820920acbd08e17ed56fe1dbade1f7741e62
yangang <1053316540@qq.com>
2017-02-15 13:22:28 +0800

    青蛙框架搭建和首页编写

--- index.ios.js
+++ index.ios.js
@@ -12,21 +12,17 @@ import {
   View
 } from 'react-native';
 
+
+/**-------导入外部的组件类---------**/
+import Main from './Component/Main/Main'
+
+
+
 export default class qingwa extends Component {
   render() {
+    
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Main/>
     );
   }
 }
