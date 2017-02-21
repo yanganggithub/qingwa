c378b716fa02e7cbe9eee58afa5b2ce206d74f9d
yangang <1053316540@qq.com>
2017-02-21 13:13:18 +0800

    排行界面搭建和加载更多的处理

--- Component/Rank/Rank.js
+++ Component/Rank/Rank.js
@@ -15,10 +15,15 @@ import {
     Image,
     Platform,   // 判断当前运行的系统
     Navigator,
-    TouchableOpacity
+    TouchableOpacity,
+    ListView,
+    ActivityIndicator
 } from 'react-native';
 
+import Dimensions from'Dimensions';
+var {width,height} = Dimensions.get('window');
 import ScrollableTabView from 'react-native-scrollable-tab-view';
+import {PullList} from 'react-native-pull';
 
 
 
@@ -27,12 +32,13 @@ import ScrollableTabView from 'react-native-scrollable-tab-view';
 export default class Rank extends Component{
     constructor(props){
         super(props);
+
+
      
     }
 
-
-
     render() {
+       
         return (
             <View style={styles.container}>
                 {/*导航*/}
@@ -44,11 +50,12 @@ export default class Rank extends Component{
                     tabBarInactiveTextColor='#262626'
                     tabBarTextStyle={{fontSize: 14}}
                     >
+                    <VideoList tabLabel="电影"  type="1"/>
+                    <VideoList tabLabel="电视剧" type="2"/>
+                    <VideoList tabLabel="综艺" type="3"/>
+                    <VideoList tabLabel="动漫" type="4"/>
 
-                    <Movie tabLabel="电影"  />
-                    <TVSeries tabLabel="电视剧" />
-                    <Variety tabLabel="综艺" />
-                    <Comic tabLabel="动漫" />
+                   
                 </ScrollableTabView>
             </View>
 
@@ -77,12 +84,312 @@ export default class Rank extends Component{
 }
 
 
+
+class VideoList extends Component {
+
+  
+
+    static defaultProps ={
+        api_url:'http://api.fffml.com/top'
+    };
+
+    constructor(props){
+        super(props);
+
+        this.cachedResults = {
+            nextPage:1,
+            items:[],
+            total:0,
+        }
+
+
+
+        this.state = {
+            // cell的数据源
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (r1, r2) => r1 !== r2
+            }),
+            isLoading:false
+        };
+    }
+
+
+
+
+
+
+
+    render() {
+        return(
+            <ListView
+                dataSource={this.state.dataSource}
+                renderRow={this.renderRow.bind(this)}
+                onEndReached={this.loadMoreData}
+                onEndReachedThreshold={20}
+                renderFooter={this.renderFooter}
+            />
+
+
+
+
+        );
+    }
+
+    renderFooter=()=>{
+        if(!this.hasMore()  && this.cachedResults.total!== 0 ){
+            return (<View style={styles.loadingMore}>
+                <Text style={styles.loadingText}>没有更多数据啦...</Text>
+            </View>);
+        }
+
+        if(!this.state.isLoading){
+            return <View style={styles.loadingMore}/>
+        }
+
+        return (
+
+            <ActivityIndicator
+                style={styles.loadingMore}
+            />
+
+        );
+    }
+
+    //加载更多的数据 上拉加载更多  滑动分页
+    loadMoreData=()=>{
+
+        if(!this.hasMore() || this.state.isLoading){
+            return
+        }
+
+        //去服务器请求加载更多的数据了
+        this.cachedResults.nextPage++;
+        this.setState({
+            isLoading:true
+        });
+
+        let page=  this.cachedResults.nextPage;
+
+        this.loadDataFromNet(page);
+
+    }
+
+    hasMore(){
+        return this.cachedResults.items.length !== this.cachedResults.total
+    }
+
+    // 请求网络数据
+    componentDidMount(){
+        console.log(this.props.type);
+        this.loadDataFromNet(1);
+    }
+
+    loadDataFromNet(page){
+        console.log('type=',this.props.type);
+
+
+        fetch(this.props.api_url,{
+            method: 'POST',
+            body: JSON.stringify({
+                type: this.props.type,
+                page: page
+            })
+        })
+            .then((response)=>response.json())
+            .then((responseData)=>{
+                // 拿到所有的数据
+                var jsonData = responseData['data'];
+                // 处理网络数据
+                this.dealWithData(jsonData);
+            })
+            .catch((error)=>{
+                 this.cachedResults.nextPage--;
+            })
+
+    }
+
+    // 处理网络数据
+    dealWithData(jsonData){
+       let items = this.cachedResults.items.slice();
+       items = this.cachedResults.items.concat(jsonData['top_list']);
+       this.cachedResults.items = items;
+        this.cachedResults.total = jsonData['count'];
+        console.log('itemsCount=',this.cachedResults.items.length);
+        // 更新状态机
+        this.setState({
+            isLoading:false,
+            // cell的数据源
+            dataSource: this.state.dataSource.cloneWithRows(this.cachedResults.items)
+        });
+
+        // console.log(headerArr, listDataArr);
+    }
+
+    renderRow(rowData){
+        return(
+            <TouchableOpacity onPress={()=>alert(0)}>
+                <View style={styles.listViewStyle}>
+                    {/*左边*/}
+                    <Image source={{uri:rowData.pic}} style={styles.imageViewStyle}/>
+                    {/*右边*/}
+
+                    <View style={styles.rightContentStyle}>
+
+                        <View style={styles.rightTopViewStyle}>
+                            <Text>{rowData.title}</Text>
+
+                        </View>
+                        <View style={styles.areaStyle}>
+                            <Text style={{color:'gray'}}>{rowData.area}</Text>
+                        </View>
+
+                        <Text style={{color:'gray'}}>{rowData.actor}</Text>
+
+                    </View>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+}
+
+
+ class PullTest extends Component {
+
+    constructor(props) {
+        super(props);
+        this.dataSource = [{
+            id: 0,
+            title: `this is the first.`,
+        }];
+        this.state = {
+
+            list: (new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})).cloneWithRows(this.dataSource),
+        };
+        this.renderHeader = this.renderHeader.bind(this);
+        this.renderRow = this.renderRow.bind(this);
+        this.renderFooter = this.renderFooter.bind(this);
+        this.loadMore = this.loadMore.bind(this);
+        this.topIndicatorRender = this.topIndicatorRender.bind(this);
+        // this.loadMore();
+    }
+
+    onPullRelease(resolve) {
+        //do something
+        setTimeout(() => {
+            resolve();
+        }, 3000);
+    }
+
+    topIndicatorRender(pulling, pullok, pullrelease) {
+        const hide = {position: 'absolute', left: -10000};
+        const show = {position: 'relative', left: 0};
+        if (pulling) {
+            this.txtPulling && this.txtPulling.setNativeProps({style: show});
+            this.txtPullok && this.txtPullok.setNativeProps({style: hide});
+            this.txtPullrelease && this.txtPullrelease.setNativeProps({style: hide});
+        } else if (pullok) {
+            this.txtPulling && this.txtPulling.setNativeProps({style: hide});
+            this.txtPullok && this.txtPullok.setNativeProps({style: show});
+            this.txtPullrelease && this.txtPullrelease.setNativeProps({style: hide});
+        } else if (pullrelease) {
+            this.txtPulling && this.txtPulling.setNativeProps({style: hide});
+            this.txtPullok && this.txtPullok.setNativeProps({style: hide});
+            this.txtPullrelease && this.txtPullrelease.setNativeProps({style: show});
+        }
+        return (
+            <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: 60}}>
+                <ActivityIndicator size="small" color="gray" />
+                <Text ref={(c) => {this.txtPulling = c;}}>当前PullList状态: pulling...</Text>
+                <Text ref={(c) => {this.txtPullok = c;}}>当前PullList状态: pullok......</Text>
+                <Text ref={(c) => {this.txtPullrelease = c;}}>当前PullList状态: pullrelease......</Text>
+            </View>
+        );
+    }
+
+    render() {
+        return (
+
+                <PullList
+                    style={{}}
+                    onPullRelease={this.onPullRelease} topIndicatorRender={this.topIndicatorRender} topIndicatorHeight={60}
+                    renderHeader={this.renderHeader}
+                    dataSource={this.state.list}
+                    pageSize={5}
+                    initialListSize={5}
+                    renderRow={this.renderRow}
+                    onEndReached={this.loadMore}
+                    onEndReachedThreshold={30}
+                    renderFooter={this.renderFooter}
+                />
+
+        );
+    }
+
+    renderHeader() {
+        return (
+            <View style={{height: 50, backgroundColor: '#eeeeee', alignItems: 'center', justifyContent: 'center'}}>
+                <Text style={{fontWeight: 'bold'}}>This is header</Text>
+            </View>
+        );
+    }
+
+    renderRow(item, sectionID, rowID, highlightRow) {
+        return (
+            <View style={{height: 50, backgroundColor: '#fafafa', alignItems: 'center', justifyContent: 'center'}}>
+                <Text>{item.title}</Text>
+            </View>
+        );
+    }
+
+    renderFooter() {
+        if(this.state.nomore) {
+            return null;
+        }
+        return (
+            <View style={{height: 50}}>
+                <ActivityIndicator />
+            </View>
+        );
+    }
+
+    loadMore() {
+        this.dataSource.push({
+            id: 0,
+            title: `begin to create data ...`,
+        });
+        for(var i = 0; i < 5; i++) {
+            this.dataSource.push({
+                id: i + 1,
+                title: `this is ${i}`,
+            })
+        }
+        this.dataSource.push({
+            id: 6,
+            title: `finish create data ...`,
+        });
+        setTimeout(() => {
+            this.setState({
+                list: this.state.list.cloneWithRows(this.dataSource)
+            });
+        }, 1000);
+    }
+
+}
+
+
+
 const styles = StyleSheet.create({
 
-    container:{
-        flex:1,
+    loadingMore:{
+
+        marginVertical:20
     },
 
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#F5FCFF',
+    },
 
     lineStyle:{
       backgroundColor:'#00ae54'
@@ -122,36 +429,42 @@ const styles = StyleSheet.create({
         bottom:Platform.OS == 'ios' ? 15:13
     },
 
+    listViewStyle:{
+        backgroundColor:'white',
+        padding:14,
+        borderBottomColor:'#e5e5e5',
+        borderBottomWidth:0.5,
+
+        flexDirection:'row'
+    },
+
+    imageViewStyle:{
+        width:80,
+        height:106
+    },
+
+    rightContentStyle:{
+        marginLeft:13,
+        width:width - 80 - 14 * 2 - 13,
+
+    },
+
+    rightTopViewStyle:{
+        marginTop:3
+    },
+    areaStyle:{
+        marginTop:7,
+        marginBottom:2
+    },
+    loadingText:{
+        fontSize:18,
+        color:'red',
+        textAlign:'center'
+    },
+
 });
 
-class Movie extends Component {
-    render() {
-        return(
-            <Text>hello</Text>
-        );
-    }
-}
-class TVSeries extends Component {
-    render() {
-        return(
-            <Text>hello</Text>
-        );
-    }
-}
 
-class Variety extends Component {
-    render() {
-        return(
-            <Text>hello</Text>
-        );
-    }
-}
 
-class Comic extends Component {
-    render() {
-        return(
-            <Text>hello</Text>
-        );
-    }
-}
+
 
